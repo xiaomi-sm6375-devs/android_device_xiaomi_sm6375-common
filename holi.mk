18342ccb709bf4ccf9e104a2bc456d346f3c6608
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    sm6375-common: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- holi.mk
+++ holi.mk
@@ -308,6 +308,12 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
