d4681d5baa88971fc7fc36a6ee836d32d938d1c5
Demon000 <demonsingur@gmail.com>
2020-07-18 18:23:51 +0300

    sm6375-common: enable ODM value additions
    Change-Id: I597f2ed5f7c7b19c0fbff96b7e610125413fd3c6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,6 +174,7 @@ TARGET_BOARD_PLATFORM := holi
 TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
