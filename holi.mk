9871d5207e0bdd4e348b2a76fa107eb91bb231d6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:06:22 +0100

    sm6375-common: Build QTI Framework detect libs
    Change-Id: Ic4db96f8bd5a19f4805bd9c40544ed51d0352c61

--- holi.mk
+++ holi.mk
@@ -206,6 +206,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
