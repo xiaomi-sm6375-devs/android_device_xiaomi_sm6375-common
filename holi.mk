b6a27c6776a75f6ed852778b6c08f61508358087
Alexander Koskovich <akoskovich@pm.me>
2022-03-22 19:56:23 -0300

    sm6375-common: bootctrl: Update to Boot Control HAL v1.2

--- holi.mk
+++ holi.mk
@@ -82,9 +82,9 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
