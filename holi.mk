c2ed7130b6948f3ce43b3085ad439d7bea532cc0
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    sm6375-common: Inherit launch_with_vendor_ramdisk for vAB
    - veux has vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- holi.mk
+++ holi.mk
@@ -25,7 +25,7 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # APEX's
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
