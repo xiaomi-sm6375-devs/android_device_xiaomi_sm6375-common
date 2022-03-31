4d8621ad0e885764f17a411b2eab4d95e0380a18
Ramii Ahmed <ramy@ahmedramy.com>
2022-03-31 11:54:36 +0000

    sm6375-common: Import compatibility matrix and manifest
    * From veux-user 11 RKQ1.211001.001 V13.0.10.0.RKCMIXM release-keys
    
    Change-Id: I4a78950e6d9ebb64794c13fd3d72b8548ce16c10

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,14 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 TARGET_BOOTLOADER_BOARD_NAME := holi
 TARGET_NO_BOOTLOADER := true
 
+# HIDL
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
