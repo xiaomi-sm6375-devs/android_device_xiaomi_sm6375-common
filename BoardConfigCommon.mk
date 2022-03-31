feaff35e9cf33a8e2e8fe5facf09edef49af94a2
Ramii Ahmed <ramy@ahmedramy.com>
2022-03-31 11:29:13 +0000

    sm6375-common: Initial skeleton
    Change-Id: I231368c54c77d559c7aa47060d041149826e16a8

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/xiaomi/sm6375-common
+
+# Inherit the proprietary files
+include vendor/xiaomi/sm6375-common/BoardConfigVendor.mk
---
+++ holi.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/sm6375-common/sm6375-common-vendor.mk)
