272269061abacf9a18db406f5699313166ce126b
Ramii Ahmed <ramy@ahmedramy.com>
2022-04-01 11:56:27 +0000

    sm6375-common: import public libraries list
    * From veux_global-user 11 RKQ1.211001.001 V13.0.10.0.RKCMIXM release-keys
    
    Change-Id: Ibb726df72d42e5836c3591bd28a836aa9b674c8e

--- holi.mk
+++ holi.mk
@@ -270,6 +270,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
