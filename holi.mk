9194250c07c463211fa1c6f8ff1e42232f9bf087
Ramii Ahmed <ramy@ahmedramy.com>
2022-04-01 22:32:23 +0000

    sm6375-common: build servicetracker HIDL
    Change-Id: I95ff25985705dfcd643379bb9f59e9f3dc41a574

--- holi.mk
+++ holi.mk
@@ -354,6 +354,12 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi_holi-multihal \
     libsensorndkbridge
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.0.vendor \
+    vendor.qti.hardware.servicetracker@1.1.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
