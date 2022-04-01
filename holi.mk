e3f2db481f8370ecebac5cdb0ac3e2d9a806b486
Ramii Ahmed <ramy@ahmedramy.com>
2022-04-01 13:34:29 +0000

    sm6375-common: build QTI thermal HIDL
    Change-Id: I03fc9b0af5fafbb67eb4fb8ca779586e39773c04

--- holi.mk
+++ holi.mk
@@ -345,6 +345,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
