8a71d7d1ccc5ecc8b1c98e279b5fce86a32a8566
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:43:20 +0200

    sm6375-common: Remove non-existing or implicitly included IPACM packages
    Change-Id: Iae06f09e101b237187638e76a6dd903ecdc2ca4f

--- holi.mk
+++ holi.mk
@@ -230,9 +230,7 @@ PRODUCT_PACKAGES += \
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # Keymaster
 PRODUCT_PACKAGES += \
