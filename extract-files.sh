f69052cc43f9263f93f9858486fbab16f2060a05
Ramii Ahmed <ramy@ahmedramy.com>
2022-09-30 23:38:55 +0000

    sm6375-common: extract-files: allow extracting proprietary-files for recovery
    * we'll be using this to extract touchscreen firmware for recovery
    
    Change-Id: I64745ff5823adee79016eabb772265aae3c06f3c

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
     extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files-recovery.txt" ]; then
+        extract "${MY_DIR}/../${DEVICE}/proprietary-files-recovery.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    fi
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
