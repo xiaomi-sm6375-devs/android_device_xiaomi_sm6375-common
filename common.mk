accb1c58383e89f13e811e5d34f52236f7e2f24e
Giovanni Ricca <giovanniricca@proton.me>
2024-09-04 22:26:30 +0200

    sm6375-common: Decommonize device-specific audio stack
    Change-Id: Ia1e65d00aac20a97b59ed2aa6a6ed2131e1632de

--- common.mk
+++ common.mk
@@ -45,7 +45,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl:32
 
 PRODUCT_PACKAGES += \
-    audio_amplifier.holi:32 \
     audio.bluetooth.default:32 \
     audio.primary.holi:32 \
     audio.r_submix.default:32 \
